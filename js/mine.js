95a2ef10e01bef8f7c8a63f16a2824607ffe4727
mohamed mohy <32440267+mohamed-mohy@users.noreply.github.com>
2021-09-19 04:17:08 +0200

    Add files via upload

---
+++ js/mine.js
@@ -0,0 +1,184 @@
+let allIcons = [
+
+                /// day icons
+                {code:1000, time: true,icon:"images/icons/day/clear-day.svg"},
+                {code:1003, time: true,icon:"images/icons/day/partly-cloudy-day.svg"},
+                {code:1006, time: true,icon:"images/icons/day/cloudy.svg"},
+                {code:1009, time: true,icon:"images/icons/day/overcast-day.svg"},
+                {code:1030, time: true,icon:"images/icons/day/mist.svg"},
+                {code:1063, time: true,icon:"images/icons/day/partly-cloudy-day-rain.svg"},
+                {code:1066, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1069, time: true,icon:"images/icons/day/partly-cloudy-day-sleet.svg"},
+                {code:1072, time: true,icon:"images/icons/day/partly-cloudy-day-drizzle.svg"},
+                {code:1087, time: true,icon:"images/icons/day/thunderstorms-day.svg"},
+                {code:1114, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1117, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1135, time: true,icon:"images/icons/day/fog.svg"},
+                {code:1147, time: true,icon:"images/icons/day/fog.svg"},
+                {code:1150, time: true,icon:"images/icons/day/rain.svg"},
+                {code:1153, time: true,icon:"images/icons/day/rain.svg"},
+                {code:1168, time: true,icon:"images/icons/day/drizzle.svg"},
+                {code:1171, time: true,icon:"images/icons/day/drizzle.svg"},
+                {code:1180, time: true,icon:"images/icons/day/partly-cloudy-day-rain.svg"},
+                {code:1183, time: true,icon:"images/icons/day/rain.svg"},
+                {code:1186, time: true,icon:"images/icons/day/partly-cloudy-day-rain.svg"},
+                {code:1189, time: true,icon:"images/icons/day/rain.svg"},
+                {code:1192, time: true,icon:"images/icons/day/partly-cloudy-day-rain.svg"},
+                {code:1195, time: true,icon:"images/icons/day/rain.svg"},
+                {code:1198, time: true,icon:"images/icons/day/rain.svg"},
+                {code:1201, time: true,icon:"images/icons/day/rain.svg"},
+                {code:1204, time: true,icon:"images/icons/day/sleet.svg"},
+                {code:1207, time: true,icon:"images/icons/day/sleet.svg"},
+                {code:1210, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1213, time: true,icon:"images/icons/day/snow.svg"},
+                {code:1216, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1219, time: true,icon:"images/icons/day/snow.svg"},
+                {code:1222, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1225, time: true,icon:"images/icons/day/snow.svg"},
+                {code:1237, time: true,icon:"images/icons/day/hail.svg"},
+                {code:1240, time: true,icon:"images/icons/day/partly-cloudy-day-rain.svg"},
+                {code:1243, time: true,icon:"images/icons/day/partly-cloudy-day-rain.svg"},
+                {code:1246, time: true,icon:"images/icons/day/partly-cloudy-day-rain.svg"},
+                {code:1249, time: true,icon:"images/icons/day/partly-cloudy-day-sleet.svg"},
+                {code:1252, time: true,icon:"images/icons/day/partly-cloudy-day-sleet.svg"},
+                {code:1255, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1258, time: true,icon:"images/icons/day/partly-cloudy-day-snow.svg"},
+                {code:1261, time: true,icon:"images/icons/day/partly-cloudy-day-hail.svg"},
+                {code:1264, time: true,icon:"images/icons/day/partly-cloudy-day-hail.svg"},
+                {code:1273, time: true,icon:"images/icons/day/thunderstorms-day.svg"},
+                {code:1276, time: true,icon:"images/icons/day/thunderstorms.svg"},
+                {code:1279, time: true,icon:"images/icons/day/thunderstorms-day-snow.svg"},
+                {code:1282, time: true,icon:"images/icons/day/thunderstorms-snow.svg"},
+
+                ///night icons
+                {code:1000, time: false,icon:"images/icons/night/clear-night.svg"},
+                {code:1003, time: false,icon:"images/icons/night/partly-cloudy-night.svg"},
+                {code:1006, time: false,icon:"images/icons/night/cloudy.svg"},
+                {code:1009, time: false,icon:"images/icons/night/overcast-night.svg"},
+                {code:1030, time: false,icon:"images/icons/night/mist.svg"},
+                {code:1063, time: false,icon:"images/icons/night/partly-cloudy-night-rain.svg"},
+                {code:1066, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1069, time: false,icon:"images/icons/night/partly-cloudy-night-sleet.svg"},
+                {code:1072, time: false,icon:"images/icons/night/partly-cloudy-night-drizzle.svg"},
+                {code:1087, time: false,icon:"images/icons/night/thunderstorms-night.svg"},
+                {code:1114, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1117, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1135, time: false,icon:"images/icons/night/fog.svg"},
+                {code:1147, time: false,icon:"images/icons/night/fog.svg"},
+                {code:1150, time: false,icon:"images/icons/night/rain.svg"},
+                {code:1153, time: false,icon:"images/icons/night/rain.svg"},
+                {code:1168, time: false,icon:"images/icons/night/drizzle.svg"},
+                {code:1171, time: false,icon:"images/icons/night/drizzle.svg"},
+                {code:1180, time: false,icon:"images/icons/night/partly-cloudy-night-rain.svg"},
+                {code:1183, time: false,icon:"images/icons/night/rain.svg"},
+                {code:1186, time: false,icon:"images/icons/night/partly-cloudy-night-rain.svg"},
+                {code:1189, time: false,icon:"images/icons/night/rain.svg"},
+                {code:1192, time: false,icon:"images/icons/night/partly-cloudy-night-rain.svg"},
+                {code:1195, time: false,icon:"images/icons/night/rain.svg"},
+                {code:1198, time: false,icon:"images/icons/night/rain.svg"},
+                {code:1201, time: false,icon:"images/icons/night/rain.svg"},
+                {code:1204, time: false,icon:"images/icons/night/sleet.svg"},
+                {code:1207, time: false,icon:"images/icons/night/sleet.svg"},
+                {code:1210, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1213, time: false,icon:"images/icons/night/snow.svg"},
+                {code:1216, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1219, time: false,icon:"images/icons/night/snow.svg"},
+                {code:1222, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1225, time: false,icon:"images/icons/night/snow.svg"},
+                {code:1237, time: false,icon:"images/icons/night/hail.svg"},
+                {code:1240, time: false,icon:"images/icons/night/partly-cloudy-night-rain.svg"},
+                {code:1243, time: false,icon:"images/icons/night/partly-cloudy-night-rain.svg"},
+                {code:1246, time: false,icon:"images/icons/night/partly-cloudy-night-rain.svg"},
+                {code:1249, time: false,icon:"images/icons/night/partly-cloudy-night-sleet.svg"},
+                {code:1252, time: false,icon:"images/icons/night/partly-cloudy-night-sleet.svg"},
+                {code:1255, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1258, time: false,icon:"images/icons/night/partly-cloudy-night-snow.svg"},
+                {code:1261, time: false,icon:"images/icons/night/partly-cloudy-night-hail.svg"},
+                {code:1264, time: false,icon:"images/icons/night/partly-cloudy-night-hail.svg"},
+                {code:1273, time: false,icon:"images/icons/night/thunderstorms-night.svg"},
+                {code:1276, time: false,icon:"images/icons/night/thunderstorms.svg"},
+                {code:1279, time: false,icon:"images/icons/night/thunderstorms-night-snow.svg"},
+                {code:1282, time: false,icon:"images/icons/night/thunderstorms-snow.svg"}
+                
+            ];
+let icon="";
+let iconNext="";
+let iconNext1="";
+/// month
+const monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+                ];
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+const d = new Date();
+
+
+async function getWeather(location = `auto:ip`)
+{
+        let http = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=7cb777ff53224ea0bb3202426211709&q=${location}&days=7`)
+    http = await http.json()
+    for (let i = 0; i < allIcons.length; i++) {
+        if(allIcons[i].code == http.current.condition.code && allIcons[i].time == http.current.condition.icon.includes("day") )
+        {
+            icon = allIcons[i].icon;
+        }else if(allIcons[i].code == http.current.condition.code && allIcons[i].time != http.current.condition.icon.includes("night")  )
+        {
+            icon = allIcons[i].icon;
+        }
+        
+    }
+    $("#imgMood").attr("src",icon)    
+    $(".main-box h4").text(http.location.name)
+    $(".day").text(weekday[http.current.is_day])
+    $(".date").text(d.getDate()+ " " + monthNames[d.getMonth()])
+    $(".dgree").text(http.current.temp_c)
+    $(".status").text(http.current.condition.text)
+    $(".Cloud").text(http.current.cloud+"%")
+    $(".Wind").text(http.current.wind_kph+" km/h")
+    $(".Wind_Dir").text(http.current.wind_dir)
+    $(".Wind_Dir").append(`<img class="imgWind" src="images/icons/barometer.svg">`);
+    $(".Humidity").text(http.current.humidity+"%")
+    $(".Sunrise").text(http.forecast.forecastday[0].astro.sunrise)
+    $(".Sunset").text(http.forecast.forecastday[0].astro.sunset)
+    $(".Moonrise").text(http.forecast.forecastday[0].astro.moonrise)
+    $(".Moonset").text(http.forecast.forecastday[0].astro.moonset)
+    $(".NxDay .day").text(weekday[http.current.is_day+1])
+    $(".NxDay .date").text(d.getDate()+1+ " " + monthNames[d.getMonth()])
+    $(".NxDay .degreemax").text(http.forecast.forecastday[1].day.maxtemp_c)
+    $(".NxDay .degreemin").text(http.forecast.forecastday[1].day.mintemp_c)
+    $(".NxDay .status").text(http.forecast.forecastday[1].day.condition.text)
+
+    for (let i = 0; i < allIcons.length; i++) {
+        if(allIcons[i].code == http.forecast.forecastday[1].day.condition.code && allIcons[i].time == http.forecast.forecastday[1].day.condition.icon.includes("day") )
+        {
+            iconNext = allIcons[i].icon;
+        }else if(allIcons[i].code == http.forecast.forecastday[1].day.condition.code && allIcons[i].time != http.forecast.forecastday[1].day.condition.icon.includes("night") )
+        {
+            iconNext1 = allIcons[i].icon;
+        }
+        
+        }
+
+    $(".NxDay .imgMood").attr("src",iconNext)
+
+    for (let i = 0; i < allIcons.length; i++) {
+        if(allIcons[i].code == http.forecast.forecastday[2].day.condition.code && allIcons[i].time == http.forecast.forecastday[2].day.condition.icon.includes("day") )
+        {
+            iconNext1 = allIcons[i].icon;
+        }else if(allIcons[i].code == http.forecast.forecastday[2].day.condition.code && allIcons[i].time != http.forecast.forecastday[2].day.condition.icon.includes("night") )
+        {
+            iconNext1 = allIcons[i].icon;
+        }
+        $(".NxDay1 .imgMood").attr("src",iconNext1)
+        $(".NxDay1 .day").text(weekday[http.current.is_day+2])
+        $(".NxDay1 .date").text(d.getDate()+2+ " " + monthNames[d.getMonth()])
+        $(".NxDay1 .degreemax").text(http.forecast.forecastday[2].day.maxtemp_c)
+        $(".NxDay1 .degreemin").text(http.forecast.forecastday[2].day.mintemp_c)
+        $(".NxDay1 .status").text(http.forecast.forecastday[2].day.condition.text)
+}
+}
+
+$("#search").keyup(function () { 
+    
+    getWeather($(this).val());
+});
+getWeather();
